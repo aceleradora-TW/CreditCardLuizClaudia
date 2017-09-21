7045f4499b6592075a7242d73f172019bba3a778
ClaudiaStrm <s_moura@hotmail.com>
2017-09-21 13:58:05 -0300

    adiciona modificações no validador

--- main.js
+++ main.js
@@ -42,29 +42,29 @@ function eMastercard(numeroDoCartao, tamanho){
   }
 }
 
-//
-function reverseNumeroDoCartao(numeroDoCartao) {
-    return numeroDoCartao.split("").reverse().join("");
+function reverseString(str) {
+    return str.split("").reverse().join("");
 }
 
 //função que valida o algoritmo de Luhn
 function validaCartao(numeroDoCartao){
   numeroDoCartao = numeroDoCartao.split(' ').join('');
-  numeroDoCartao = reverseNumeroDoCartao(numeroDoCartao);
-  console.log(numeroDoCartao);
+
   if (tipoDoCartao(numeroDoCartao)) {
+    numeroCartaoInvertido = reverseString(numeroDoCartao);
+
     let soma = 0;
     let dobra = 0;
-    for(let i = 0; i<numeroDoCartao.length; i++){
-      if(i % 2 === 0){
-        dobra = numeroDoCartao.charAt(i) * 2;
+    for(let i = 0; i < numeroCartaoInvertido.length; i++){
+      if(i % 2 !== 0){
+        dobra = numeroCartaoInvertido.charAt(i) * 2;
         soma += dobra;
         //verifica se o número dobrado é maior que 9
         if (dobra > 9) {
           soma -= 9;
         }
       } else {
-        soma += parseInt(numeroDoCartao.charAt(i));
+        soma += parseInt(numeroCartaoInvertido.charAt(i));
       }
     }
     if (soma % 10 === 0) {
@@ -77,8 +77,4 @@ function validaCartao(numeroDoCartao){
 
 
 console.log(validaCartao('4408 0412 3456 7893')); //Visa
-console.log(validaCartao('4408 0412 3436 7893')); //Cartão Inválido
-console.log(validaCartao('6011 4600 0000 0000')); //Discover
-console.log(validaCartao('5207 0000 0000 0000'));//Mastercard
-console.log(validaCartao('3707 0000 0000 000'));//Amex
-console.log(validaCartao('3707 0000 0000 0000'));//Cartão Inválido
+console.log(validaCartao('4417 1234 5678 9112')); //Cartão Inválido
